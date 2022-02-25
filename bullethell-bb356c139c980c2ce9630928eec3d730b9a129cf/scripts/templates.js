b79c01621f163f87b9a1bfd90b8bd06d8a52ea11
Captain Chicky <maxfang2015@yahoo.com>
2022-02-24 21:25:02 -0500

    Update templates.js

--- bullethell-bb356c139c980c2ce9630928eec3d730b9a129cf/scripts/templates.js
+++ bullethell-bb356c139c980c2ce9630928eec3d730b9a129cf/scripts/templates.js
@@ -18,32 +18,6 @@ MODEL.player = function(e) {
     image.onload = function(){
       context.drawImage(image, 0, 0);
     }
-    
-    //------------------------------
-    //push();
-    //translate(e.pos.x, e.pos.y);
-
-    //// Thruster
-    //fill('#7C8A99');
-    //stroke(0);
-    //rectMode(RADIUS);
-    //rect(-0.5, 4, 6, 10);
-
-    //// Rear fins
-    //fill('#ACBAC9');
-    //triangle(3, 2, 3, 10, 22, 16);
-    //triangle(-3, 2, -3, 10, -22, 16);
-
-    //// Wings
-    //fill('#BDC3C7');
-    //triangle(1, 3, 1, -24, 12, 6);
-    //triangle(-1, 3, -1, -24, -12, 6);
-
-    //// Canopy
-    //fill('#29C5FF');
-    //ellipse(0, 1, 6, 8);
-
-    //pop();
 };
 
 
